1f01de4047d818f15d378c8fc49f177b47a44324
Ahmed Ayad <ahmed.ibrahim01974@gmail.com>
2025-10-30 17:27:32 +0300

    Updated app settings Added promotions features

---
+++ Backend/Cartify.Application/Services/Implementation/Merchant/MerchantPromotionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartify.Application.Services.Implementation.Merchant
+{
+    public class MerchantPromotionService
+    {
+    }
+}
